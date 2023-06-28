160da9bb521a1fdf701ac39e5d76e243d20bf07b
Miguel Garcia <miguelfiuza315@gmail.com>
2023-06-28 03:10:50 -0300

    adadwd

--- api/db.js
+++ api/db.js
@@ -1,8 +1,8 @@
 import mysql from "mysql"
 
 export const db = mysql.createConnection({
-    host: "containers-us-west-208.railway.app",
-    user: "root",
-    password: "CLlp0RW9FaePcUUyexoG",
-    database: "railway"
+    host: "bc9upqx0nbamjnpolfnl-mysql.services.clever-cloud.com",
+    user: "ueegjw6lakm9lbcp",
+    password: "032QL7MNmmYbLmD6xhdR",
+    database: "bc9upqx0nbamjnpolfnl"
 })
