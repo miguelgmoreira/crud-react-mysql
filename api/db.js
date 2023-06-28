ef0ae9556edf54d6fd40b66e574d6529c30937f6
Miguel Garcia <miguelfiuza315@gmail.com>
2023-06-28 02:52:10 -0300

    adfada

--- api/db.js
+++ api/db.js
@@ -1,8 +1,8 @@
 import mysql from "mysql"
 
 export const db = mysql.createConnection({
-    host: "bc9upqx0nbamjnpolfnl-mysql.services.clever-cloud.com",
-    user: "ueegjw6lakm9lbcp",
-    password: "032QL7MNmmYbLmD6xhdR",
-    database: "bc9upqx0nbamjnpolfnl"
+    host: "localhost",
+    user: "root",
+    password: "miguel123",
+    database: "crud"
 })
