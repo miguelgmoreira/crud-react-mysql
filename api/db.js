bc9e4d33298aa33fd1ca39f4ca2e601ef7a0bd0d
Miguel Garcia <miguelfiuza315@gmail.com>
2023-06-28 03:47:54 -0300

    adwdwd

--- api/db.js
+++ api/db.js
@@ -1,8 +1,8 @@
 import mysql from "mysql"
 
 export const db = mysql.createConnection({
-    host: "containers-us-west-208.railway.app",
+    host: "containers-us-west-209.railway.app",
     user: "root",
-    password: "CLlp0RW9FaePcUUyexoG",
+    password: "NAq62PWvnEuwgH7r6UEO",
     database: "railway"
 })
